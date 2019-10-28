6deb7057bf84249fb9453110f9a93969ebb3c343
Weverton do Couto Timoteo <weverton.ct@gmail.com>
2019-10-28 15:30:05 -0300

    Makes arg optional in Normalizers::Extension

--- lib/cc/analyzer/normalizers/extension.rb
+++ lib/cc/analyzer/normalizers/extension.rb
@@ -4,7 +4,7 @@ module CC
       class Extension
         attr_reader :name, :extensions, :code_path
 
-        def initialize(name, available_extensions = [], code_path)
+        def initialize(name, available_extensions = [], code_path = "")
           @name = name
           @extensions = available_extensions
           @code_path = code_path
