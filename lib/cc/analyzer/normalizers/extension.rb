0bf7c28292266f81054b491a6fd2e1483ff109a0
Weverton do Couto Timoteo <weverton.ct@gmail.com>
2019-10-28 15:21:14 -0300

    Load extension normalizer dynamically

---
+++ lib/cc/analyzer/normalizers/extension.rb
@@ -0,0 +1,34 @@
+module CC
+  module Analyzer
+    module Normalizers
+      class Extension
+
+        attr_reader :name, :extensions, :code_path
+
+        def initialize(name, available_extensions = [], code_path)
+          @name = name
+          @extensions = available_extensions
+          @code_path = code_path
+        end
+
+        def call
+          if Kernel.const_defined?(engine_normalizer_const)
+            engine_normalizer_klass = Kernel.const_get(engine_normalizer_const)
+
+            engine_normalizer_klass.new(extensions, code_path).call
+          end
+        end
+
+        private
+
+        def namespace
+          self.class.to_s.deconstantize
+        end
+
+        def engine_normalizer_const
+          "#{namespace}::Extensions::#{name.capitalize}Extension"
+        end
+      end
+    end
+  end
+end
---
+++ spec/cc/analyzer/normalizers/extension_spec.rb
@@ -0,0 +1,20 @@
+require "spec_helper"
+
+module CC::Analyzer::Normalizers
+  describe Extension do
+    describe "#run" do
+      it "ignores engine when there is no normalizer for it" do
+        extension = Extension.new("reek", [], "/tmp/code")
+
+        expect { extension.call }.to_not raise_error
+      end
+
+      it "calls normalizer class for engine" do
+        extension = Extension.new("rubocop", [], "/tmp/code")
+
+        expect_any_instance_of(CC::Analyzer::Normalizers::Extensions::RubocopExtension).to receive(:call)
+        extension.call
+      end
+    end
+  end
+end
